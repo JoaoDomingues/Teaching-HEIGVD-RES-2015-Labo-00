6988ba5a04d87b4722374062845a9873c49c2bc6
JoaoDomingues <joo.dominguespedrosa@heig-vd.ch>
2015-02-20 21:54:16 +0100

    Implementation of lab00 according to specification

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.heigvd.res.lab00;
 
 /**
  *
- * @author Domingues
+ * @author Joao Domingues
  */
 public interface IInstrument {
    public int getSoundVolume();
